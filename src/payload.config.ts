50e44c5eeea52f4d61ba53ae4cbba17f9250f2c5
rahul satasiya <rahulsatasiya@gmail.com>
2026-02-20 14:20:13 +0530

    Fix user role permissions

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -86,15 +86,9 @@ export default buildConfig({
   jobs: {
     access: {
       run: ({ req }: { req: PayloadRequest }): boolean => {
-        // Allow logged in users to execute this endpoint (default)
         if (req.user) return true
-
         const secret = process.env.CRON_SECRET
         if (!secret) return false
-
-        // If there is no logged in user, then check
-        // for the Vercel Cron secret to be present as an
-        // Authorization header:
         const authHeader = req.headers.get('authorization')
         return authHeader === `Bearer ${secret}`
       },
